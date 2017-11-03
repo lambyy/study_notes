3579a61f221f117354e3750e23b8e1be9ed424d4
Betty <b.lam4392@gmail.com>
2017-11-02 18:29:51 -0700

    BST practical

--- BinarySearchTree/lib/bst_practical.rb
+++ BinarySearchTree/lib/bst_practical.rb
@@ -1,11 +1,5 @@
 require_relative 'binary_search_tree.rb'
 
 def kth_largest(tree_node, k)
-  array = []
-  until array.length == k
-    max = BinarySearchTree.maximum(tree_node)
-    array.push(max.value)
-    tree_node.delete(max.value)
-  end
-  array.last
+  
 end
